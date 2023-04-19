fd18547509083f6e07f8faeaba8f0edeb5001576
juani <juangarnero@hotmail.com.ar>
2023-04-19 11:56:06 -0300

    add Entitie/Product

---
+++ Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace BienenstockCorpAPI
+{
+    public class Product
+    {
+        
+       private int Id_product { get; set; }
+       private string name { get; set; }
+       private DateTime dateIn { get; set; }
+       private float price { get; set; }
+
+       public Product() { }
+
+       public Product(Id_product,name,dateIn,price) {
+            this.Id_product = Id_product;
+            this.name = name;
+            this.dateIn = dateIn;
+            this.price = price;
+       }
+
+
+
+    }
+
+}
+
