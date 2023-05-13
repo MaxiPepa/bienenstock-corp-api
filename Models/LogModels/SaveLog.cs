42d835b38da27aeb045960c3ae8bae821c9dc8ee
juani <juangarnero@hotmail.com.ar>
2023-05-13 20:40:24 -0300

    Package Log Done

---
+++ Models/LogModels/SaveLog.cs
@@ -0,0 +1,14 @@
+﻿namespace BienenstockCorpAPI.Models.LogModels
+{
+    public class SaveLogRequest
+    {
+        public string Description { get; set; }
+        public int UserId { get; set; }
+    }
+
+    public class SaveLogResponse
+    {
+        public string Message { get; set; }
+        public bool Success { get; set; }
+    }
+}
---
+++ Services/LogService.cs
@@ -0,0 +1,69 @@
+﻿using BienenstockCorpAPI.Data.Entities;
+using BienenstockCorpAPI.Data;
+using BienenstockCorpAPI.Models.LogModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace BienenstockCorpAPI.Services
+{
+    public class LogService
+    {
+        #region Constructor
+        private readonly BienenstockCorpContext _context;
+
+        public LogService(BienenstockCorpContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region Log
+        public async Task<GetLogsResponse> GetLogs()
+        {
+            var logs = await _context.Log
+                .Include(x => x.User)
+                .ToListAsync();
+
+            return new GetLogsResponse
+            {
+                Logs = logs.Select(l => new GetLogsResponse.LogItem
+                {
+                    LogId = l.LogId,
+                    UserFullName = l.User.Name  + " " + l.User.LastName,
+                    Description = l.Description,
+                    Date = l.Date,
+                }).OrderBy(x => x.Date).ToList(),
+            };
+        }
+
+        public async Task<SaveLogResponse> CreateLog(SaveLogRequest rq)
+        {
+            var logItem = new Log
+            {
+                Description = rq.Description,
+                Date = DateTime.Now,
+                UserId = rq.UserId,
+            };
+
+            try
+            {
+                _context.Log.Add(logItem);
+                await _context.SaveChangesAsync();
+
+                return new SaveLogResponse
+                {
+                    Message = "Log successfully created",
+                    Success = true
+                };
+            }
+            catch (Exception ex)
+            {
+                return new SaveLogResponse
+                {
+                    Message = ex.Message,
+                    Success = false,
+                };
+            }
+        }
+        #endregion
+    }
+}
