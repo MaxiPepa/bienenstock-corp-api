0abe8e087ac50fbadda13a4416802695c8361598
MaxiPepa <maximopepa@gmail.com>
2023-06-04 23:57:49 -0300

    Fixed CompanyStatsResponse

--- Models/ReportModels/GetCompanyStats.cs
+++ Models/ReportModels/GetCompanyStats.cs
@@ -6,9 +6,9 @@
         public string? Message { get; set; }
         public int TotalProducts { get; set; }
         public int TotalUsers { get; set; }
-        public int TotalPurchases { get; set; }
-        public int TotalSales { get; set; }
-        public int TotalPendingProducts { get; set; }
+        public int CompletedPurchases { get; set; }
+        public int CompletedSales { get; set; }
+        public int PendingTransactions { get; set; }
         public int TotalReports { get; set; }
     }
 }
