dec731189530352a30994d7fb3edfbf3cae4b087
MaxiPepa <maximopepa@gmail.com>
2023-06-09 23:19:31 -0300

    Added userId to login response

--- Models/AuthenticationModels/Login.cs
+++ Models/AuthenticationModels/Login.cs
@@ -9,6 +9,7 @@
     public class LoginResponse
     {
         public string Token { get; set; } = null!;
+        public int UserId { get; set; }
         public string? Avatar { get; set; }
         public string Email { get; set; } = null!;
         public string FullName { get; set; } = null!;
