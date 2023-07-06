1589f091daffb0e2c8b405abb36728b537feab74
MaxiPepa <maximopepa@gmail.com>
2023-07-05 21:44:26 -0300

    Added StatisticsReport to REportService

---
+++ Models/ReportModels/GetStatistics.cs
@@ -0,0 +1,27 @@
+﻿namespace BienenstockCorpAPI.Models.ReportModels
+{
+    public class GetStatisticsResponse
+    {
+        public List<Item> Sales { get; set; } = null!;
+        public List<Item> Purchases { get; set; } = null!;
+        public List<ProductItem> Products { get; set; } = null!;
+        public int CancelledSales { get; set; }
+        public int CancelledPurchases { get; set; }
+        public decimal TotalSaleIncome { get; set; }
+        public decimal TotalPurchaseIncome { get; set; }
+        public string? Message { get; set; }
+        public bool Success { get; set; }
+
+        public class Item
+        {
+            public string? Date { get; set; }
+            public int Quantity { get; set; }
+        }
+
+        public class ProductItem
+        {
+            public string? Name { get; set; }
+            public int Quantity { get; set; }
+        }
+    }
+}
--- Services/ReportService.cs
+++ Services/ReportService.cs
@@ -3,6 +3,7 @@ using BienenstockCorpAPI.Helpers;
 using System.Security.Claims;
 using BienenstockCorpAPI.Models.ReportModels;
 using Microsoft.EntityFrameworkCore;
+using BienenstockCorpAPI.Helpers.Consts;
 
 namespace BienenstockCorpAPI.Services
 {
@@ -58,6 +59,63 @@ namespace BienenstockCorpAPI.Services
                 TotalReports = 0,
                 Success = true,
                 Message = "Successfully retrieved stats",
+            };
+        }
+
+        public async Task<GetStatisticsResponse> GetStatistics(ClaimsIdentity? identity)
+        {
+            var token = identity.TokenVerifier();
+
+            if (!token.Success || (token.UserType != UserType.ADMIN && token.UserType != UserType.ANALYST))
+            {
+                return new GetStatisticsResponse
+                {
+                    Message = "Inssuficient permissions",
+                    Success = false,
+                };
+            }
+
+            var purchases = await _context.Purchase
+                .Include(x => x.ProductPurchases)
+                .ToListAsync();
+
+            var sales = _context.Sale
+                .Include(x => x.ProductSales)
+                .AsQueryable();
+
+            var products = _context.ProductSale
+                .GroupBy(x => x.ProductId)
+                .Select(y => new
+                {
+                    Product = y.Key,
+                    ProductName = y.First().Product.Name,
+                    TotalQuantity = y.Sum(p => p.Quantity),
+                })
+                .OrderByDescending(x => x.TotalQuantity);
+
+            return new GetStatisticsResponse
+            {
+                Purchases = purchases.Where(x => !x.Pending && !x.Cancelled).Select(x => new GetStatisticsResponse.Item
+                {
+                    Date = x.Date.Date.ToString("yyyy-MM-dd"),
+                    Quantity = x.ProductPurchases.Sum(x => x.Quantity),
+                }).ToList(),
+                Sales = sales.Where(x => x.Dispatched).Select(x => new GetStatisticsResponse.Item
+                {
+                    Date = x.Date.Date.ToString("yyyy-MM-dd"),
+                    Quantity = x.ProductSales.Sum(x => x.Quantity),
+                }).ToList(),
+                Products = products.Select(x => new GetStatisticsResponse.ProductItem
+                {
+                    Name = x.ProductName,
+                    Quantity = x.TotalQuantity,
+                }).ToList(),
+                CancelledPurchases = purchases.Where(x => x.Cancelled).ToList().Count,
+                CancelledSales = purchases.Where(x => x.Cancelled).ToList().Count,
+                TotalPurchaseIncome = purchases.Sum(x => x.TotalPrice),
+                TotalSaleIncome = sales.Sum(x => x.TotalPrice),
+                Success = true,
+                Message = "Successfully retrieved statistics",
             };
         }
         #endregion
